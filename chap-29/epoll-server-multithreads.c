46f8ea639fbe15f515471320f7878d2ead61e9db
cement <735490850@qq.com>
2020-03-23 11:45:55 +0800

    添加 注释

--- chap-29/epoll-server-multithreads.c
+++ chap-29/epoll-server-multithreads.c
@@ -1,3 +1,10 @@
+/*
+ * @Author: Cement
+ * @Date: 2020-03-18 20:05:16
+ * @LastEditors: Cement
+ * @LastEditTime: 2020-03-20 00:35:49
+ * @Description: 
+ */
 #include <lib/acceptor.h>
 #include "lib/common.h"
 #include "lib/event_loop.h"
@@ -45,17 +52,17 @@ int onConnectionClosed(struct tcp_connection *tcpConnection) {
 }
 
 int main(int c, char **v) {
-    //主线程event_loop
+    //主线程event_loop ， 即acceptor线程？ 
     struct event_loop *eventLoop = event_loop_init();
 
-    //初始化acceptor
+    //初始化acceptor，函数里面创建了lfd，绑定端口，设置连接上限，开始监听
     struct acceptor *acceptor = acceptor_init(SERV_PORT);
 
     //初始tcp_server，可以指定线程数目，这里线程是4，说明是一个acceptor线程，4个I/O线程，没一个I/O线程
-    //tcp_server自己带一个event_loop
+    //tcp_server自己带一个event_loop，就是上面的eventLoop，再带一个线程池
     struct TCPserver *tcpServer = tcp_server_init(eventLoop, acceptor, onConnectionCompleted, onMessage,
                                                   onWriteCompleted, onConnectionClosed, 4);
-    tcp_server_start(tcpServer);
+    tcp_server_start(tcpServer); 
 
     // main thread for acceptor
     event_loop_run(eventLoop);
